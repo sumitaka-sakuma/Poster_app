707ba2d7cb0ee0ae95059cd66a7d557f0fe3be93
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-10 23:52:32 +0900

    Models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'text'
+    ];
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    
+    use SoftDeletes;
+
+    protected $fillable = [
+        'text'
+    ];
 }
